96a201c7790aaf36f08600a8d313d19ea03b1437
Tismey <108024662+Tismey@users.noreply.github.com>
2024-11-03 21:41:14 +0100

    Update Ak47Script.cs

--- OverSeer/Assets/Weapons/Scripts/Ak47Script.cs
+++ OverSeer/Assets/Weapons/Scripts/Ak47Script.cs
@@ -9,7 +9,7 @@ public class Ak47Script : WeaponAbstract
     public override void ShotBehavior(Vector3 dir)
     {
         var b  = Instantiate(bullet, muzzle.position - owner.transform.forward * 4, muzzle.rotation);
-        b.GetComponent<Rigidbody>().AddForce(dir * 5000f);
+        b.GetComponent<Rigidbody>().AddForce(dir * 10000f);
     }
 
     public override void ReloadBehavior()
